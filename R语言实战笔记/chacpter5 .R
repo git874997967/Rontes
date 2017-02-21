fb3dc366011e1cf03b626bb6b1a46958624acd3b
git874997967 <g874997967@gmail.com>
2017-02-20 23:46:23 -0800

    chacpter5
    继续第五章 学习

--- R语言实战笔记/chacpter5 .R
+++ R语言实战笔记/chacpter5 .R
@@ -34,5 +34,35 @@ head(mydata,n=10)
 #string methods
 paste("x",1:3,seq="M")
 paste("today is ",date())
-
 paste("x",1:4,seq=FALSE)
+
+options(digit=2)#  数字全部用两位小数表示
+Student=c("J S","A W","B M","D J","J M","C C","R Y","G K","J E","M R")
+Math=c(502,600,412,358,495,512,410,625,573,522)
+Science=c(95,99,80,82,75,85,80,95,89,86)
+English=c(25,22,18,15,20,28,15,30,27,18)
+roster=data.frame(Student,Math,Science,English,stringsAsFactors = FALSE)
+roster
+
+z=scale(roster[,2:4])
+score=apply(z,1,mean)
+roster=cbind(roster,score)
+y=quantile(score,c(.8,.6,.4,.2))
+roster=within(roster,{
+  roster$grade[score>=y[1]]="A"
+  roster$grade[score<y[1]&score>=y[2]]="B"
+  roster$grade[score<y[2]&score>=y[3]]="C"
+  roster$grade[score<y[3]&score>=y[4]]="D"
+  roster$grade[score<y[4]]="F"
+})
+name=strsplit((roster$Student)," ")
+lastName=sapply(name,"[",2)
+firstName=sapply(name,"[",1)
+roster=cbind(firstName,lastName,roster[,-1])# 有了详细的姓名 不用student  列了 
+roster=roster[order(firstName,lastName),]
+
+roster
+
+
+
+
