c8455c42a2d0992982221ea134e05f4d10800d55
git874997967 <g874997967@gmail.com>
2017-03-19 00:05:47 -0700

    update
    轴须图   核密图   rgb 透明色

--- R语言实战笔记/chap6.r
+++ R语言实战笔记/chap6.r
@@ -4,17 +4,17 @@ library(plotrix)
 counts=table(Arthritis$Improved)
 counts
 head(Arthritis)
- 
-     barplot(counts,main="Simple bar plot",
-             xlab="Improvement", ylab=" Frequncy")
-     barplot(counts,main="Simple bar plot",horiz=TRUE,
-             xlab="Improvement", ylab=" Frequncy")
-     with(Arthritis,
-          plot(Improved, main="this is title",
-                  xlab="this is xlab",
-                  ylab="this is ylab",
-                  horiz = TRUE)
-          )
+
+barplot(counts,main="Simple bar plot",
+        xlab="Improvement", ylab=" Frequncy")
+barplot(counts,main="Simple bar plot",horiz=TRUE,
+        xlab="Improvement", ylab=" Frequncy")
+with(Arthritis,
+     plot(Improved, main="this is title",
+          xlab="this is xlab",
+          ylab="this is ylab",
+          horiz = TRUE)
+)
 counts=table(Arthritis$Treatment,Arthritis$Improved,)
 counts
 barplot(counts)
@@ -49,5 +49,41 @@ pie(slices,labels =  lbls , col=rainbow(length(lbls)),main="simple chart")
 pie3D(slices ,labels=lbls,explode=0.1,main="3D chart")
 fan.plot(slices,labels=lbls,main="fan plot",cex.name=0.5)
 
+hist(slices)
+par(mfrow=c(2,2))
+hist(mtcars$mpg)
+with(mtcars,(
+  hist(
+    mpg,breaks=12, col="red",xlab=" mpg",main=" colored mpg with 12 bins"
+  )
+))
+with(mtcars,(
+  hist(mpg,freq=FALSE, breaks=12,col="green",xlab="Mpg",main=" his of mpg ")
+),
+rug(jitter(mpg)),
+lines(density(mpg),col="blue",lwd=2)
+)
+
+x=mtcars$mpg
+h=with(mtcars,hist(mpg,breaks=12,col="blue",xlab="mpg",main="hist of mpg of mtcars"))
+
+# error happens on length
+xfit=seq(min(x),max(x),length-40)
+yfit=dnorm(xfit,mean=mean(x),sd=sd(x))
+yfit=yfit*diff(h$mids[1:2])*length(x)
+lines=(xfit,yfit,col="green",lwd=2)
+box()
+
+#核密度图
+h=density(head(mtcars$mpg))
+t=density(tail(mtcars$mpg))
+
+par(mfrow=c(2,1))
+
+plot(d,main="kernel density of miles per gallon")
+polygon(d,col= rgb(255, 0, 0, 80, maxColorValue=255),border = "blue")
+polygon(t,col= rgb(0, 255, 0, 80, maxColorValue=255) ,border = "blue")
+rug(mtcars$mpg,col="brown")
+mtcars$mpg
 
 
