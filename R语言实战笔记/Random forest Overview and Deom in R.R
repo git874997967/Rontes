cd05a03da25938a3815a47b3c2984b4c298d3c38
git874997967 <g874997967@gmail.com>
2017-03-25 23:16:33 -0700

    Random forest overview

---
+++ R语言实战笔记/Random forest Overview and Deom in R.R
@@ -0,0 +1,13 @@
+# random forest
+# avoid the problem of overfitting 
+library(randomForest)
+s=sample(2,nrow(iris),replace=TRUE,prob=c(0.85,0.15))
+train=iris[s==1,]
+test=iris[s==2,]
+rfm=randomForest(Species~., data=train,do.trace=TRUE,ntree=500)
+predict5=predict(rfm,test)
+table(test[,5],predict5)
+importance(rfm)
+mean(test[,5]==predict5)
+getTree(rfm,500,labelVar = TRUE)
+ 
