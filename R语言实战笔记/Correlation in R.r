8a8dcc44dc89bb53c5f91874e4ea3adbef01a61b
git874997967 <g874997967@gmail.com>
2017-03-25 23:18:21 -0700

    correlation in R
    相关性的补充

--- R语言实战笔记/Correlation in R.r
+++ R语言实战笔记/Correlation in R.r
@@ -2,6 +2,7 @@
 library(GGally)
 library(pcaPP)
 library(corrplot)
+library(ca)
 d=iris
 dd=mtcars
 head(d)
@@ -28,3 +29,15 @@ diag(2.1, nr = 3, nc = 5)
  layout(m, widths=c(2, 1),
          heights=c(1, 2))
  layout.show(2)
+ 
+ 
+ mytable=with(iris,table(iris$Species,iris$Sepal.Length))
+prop.table(mytable,1) 
+fit=ca(mytable)
+print(fit) 
+summary(fit)
+plot(fit) 
+plot(fit, mass = TRUE, contrib = "absolute", map =
+       "rowgreen", arrows = c(FALSE, TRUE))
+
+head(diamonds)
