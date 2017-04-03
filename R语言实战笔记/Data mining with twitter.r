14f6309f6d6be915186371cccaa0e845de227c3c
git874997967 <g874997967@gmail.com>
2017-04-02 22:21:30 -0700

    ggplot2 scatter plot

--- R语言实战笔记/Data mining with twitter.r
+++ R语言实战笔记/Data mining with twitter.r
@@ -1,1 +1,22 @@
+ library(ggplot2)
+ library(devtools)
+ library(Rcpp)
+ install_github('ramnathv/rCharts')
+ install_github("madlogos/recharts") 
+ library(rCharts)
+ library(recharts)
+ library(d3r)
+ #basic scatter plot
+ qplot(mpg,wt,data=mtcars)
+ names(mtcars) = gsub("\\.", "", names(mtcars))
+ rPlot(mpg ~ wt , data = mtcars, color = 'wt', type = 'point')
+ # change the shape of the point with ggplot2
+ #  use factor function will have a better effect when
+ #the size is not too big 
+ facr=factor(mtcars$cyl)
+ p= ggplot(mtcars,aes(x=mpg,y=wt,color=facr))
+ 
+ p+ geom_point()+theme_classic()+ggtitle("scatter plot in ggplot2")+xlab("mpgg")+ylab("weight")+labs("cylinders")
+ 
+ 
  
