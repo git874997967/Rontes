744b89ef64b53c896169c4b284c95f4baf10b738
git874997967 <g874997967@gmail.com>
2017-11-11 00:19:48 -0800

    开始学习 数据清理相关知识

--- R语言实战笔记/Random forest Overview and Deom in R.R
+++ R语言实战笔记/Random forest Overview and Deom in R.R
@@ -4,10 +4,11 @@ library(randomForest)
 s=sample(2,nrow(iris),replace=TRUE,prob=c(0.85,0.15))
 train=iris[s==1,]
 test=iris[s==2,]
-rfm=randomForest(Species~., data=train,do.trace=TRUE,ntree=500)
+rfm=randomForest(Species~., data=train,do.trace=TRUE,ntree=146)
 predict5=predict(rfm,test)
+summary(predict5)
 table(test[,5],predict5)
 importance(rfm)
 mean(test[,5]==predict5)
 getTree(rfm,500,labelVar = TRUE)
- 
+plot(rfm) 
--- R语言实战笔记/test.R
+++ R语言实战笔记/test.R
@@ -1,4 +1,8 @@
- ######data cleaning
+library(lubridate) 
+library(stringr)
+library(editrules)
+library(deducorrect)
+######data cleaning
  # type checking   normalizing
  # fix and impute
  # estimate analyze derive
@@ -94,3 +98,104 @@ M3=transform(M3,birth=as.numeric(birth),
              dead=as.numeric(dead))
 M3
 as.numeric(c("7", "7*", "7.0", "7,0"))
+gender=c(2,1,1,0,2,0,1,1)
+recode=c(other=0,male=1,female=2)
+names(recode)
+genderF=factor(gender,levels=recode,labels=names(recode))
+genderF
+current_time=Sys.time()
+class(current_time)
+Sys.timezone()
+dates= c("15/02/2013", "15 Feb 13", "It happened on 15 02 '13")
+dmy(dates)
+dmy("15 Febr. 2013")
+dates='28-02-2011'
+as.POSIXct(dates,format='%d-%m-%Y')
+gender
+ 
+GenderEx=c('M ','male ','FEMALE','Fem.','male')
+GenderEx1=str_trim(GenderEx)
+GenderEx2=str_to_lower(GenderEx1)
+codes=c('male','female')
+Data=adist(GenderEx2,codes)
+colnames(Data)=codes
+rownames(Data)=GenderEx2
+i=apply(Data,1,which.min)
+data.frame(final=codes[i])
+#Sys.getlocale("LC_CTYPE")
+v <- factor(c("2", "3", "5", "7", "11",'44546'))
+v1=as.character(v)
+v2=as.numeric(v)
+v3=strtoi(v)
+class(v3) 
+class(v3[5])
+exer=readLines(file.choose(),encoding = 'utf-8')
+commont=grepl("^/",exer)
+commonts=exer[commont]
+info=exer[!commont]
+ str_trim(info)
+info=str_to_lower(info) 
+info[2]=paste(info[2],'NA')
+infoList=strsplit(info,';')
+infoList 
+infom=matrix(unlist(infoList),byrow=T,ncol=3)
+df=data.frame(infom)
+colname=c('gender','age','weight')
+colnames(df)=colname
+df
+dis=adist(df$gender,codes)
+dis
+i=apply(dis,1,which.min)
+i
+df$gender=codes[i]
+str(df)
+df$age=as.numeric(df$age)
+#### next part process to the consistent data
+# 1 detection of an inconsistency 
+age=c(23,21,NA)
+mean(age)
+mean(age,na.rm=T)
+x <- c(1:10, 20, 30)
+boxplot.stats(x)
+x
+data=read.csv(file.choose(),encoding = 'utf-8')
+class(data)
+## use editrules to set check rules
+(E <- editset(c("age >=0", "age <= 150","yearsmarried >=0", "yearsmarried <= 50")))
+F=editfile(file.choose())
+F
+ve=violatedEdits(F,data)
+data[1,]$status %in% c('married','single','widowed')
+summary(ve)
+plot(F)
+id=c(2,5)
+data[id,]
+violatedEdits(F,data[id,])
+le=localizeErrors(F,data[id,],method='mip')
+le$adapt
+data[2, "status"] <- "single"
+data[5, "height"] <- 7
+data[5, "agegroup"] <- "adult"
+summary(violatedEdits(E, data[id, ]))
+#####  unifiy the height
+marx=read.csv(file.choose(),sep=' ')
+marx_m=marx
+I ='cm'
+marx_m[I,'height']=marx$height[I]/100
+marx_m
+###########dedu correct 
+G=editmatrix(expression(
+  staff+cleaning+housing==total,
+  staff>=0,
+  housing>=0,
+  cleaning>=0
+))
+dat=data.frame(
+  staff=c(100,100,100),
+  housing=c(NA,50,NA),
+  cleaning=c(NA,NA,NA),
+  total=c(100,180,NA)
+)
+str(dat)
+cor=deduImpute(G,dat)
+cor$corrected
